b34717a09708e447e744d3b8526933d32736e8db
Santiago Giraldo <26585484+tiagogiraldo@users.noreply.github.com>
2025-07-10 09:31:50 -0500

    Add files via upload
    utils

--- utils/state_utils.py
+++ utils/state_utils.py
@@ -1,1 +1,9 @@
- 
+from typing import TypedDict, Annotated, Union, List, Dict
+from langchain_core.messages import BaseMessage
+import operator
+
+
+# Define Agent State
+class AgentState(TypedDict):
+    messages: Annotated[List[BaseMessage], operator.add]
+    output: Union[Dict, None] = None
